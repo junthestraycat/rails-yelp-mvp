1c8042130dfb25176051c21095fefca54dd2ebee
Junichi Masuda <junichimasuda@outlook.com>
2021-11-10 14:36:59 +0100

    all green

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,14 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: %i[ show edit update destroy ]
+  before_action :set_restaurant, only: [:chef,:show, :edit, :update, :destroy]
 
-  # GET /restaurants or /restaurants.json
+  # GET /restaurants
   def index
     @restaurants = Restaurant.all
   end
 
-  # GET /restaurants/1 or /restaurants/1.json
+  # GET /restaurants/1
   def show
+    @reviews = @restaurant.reviews
   end
 
   # GET /restaurants/new
@@ -19,50 +20,43 @@ class RestaurantsController < ApplicationController
   def edit
   end
 
-  # POST /restaurants or /restaurants.json
+  # POST /restaurants
   def create
     @restaurant = Restaurant.new(restaurant_params)
 
-    respond_to do |format|
-      if @restaurant.save
-        format.html { redirect_to @restaurant, notice: "Restaurant was successfully created." }
-        format.json { render :show, status: :created, location: @restaurant }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
-      end
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /restaurants/1 or /restaurants/1.json
+  # PATCH/PUT /restaurants/1
   def update
-    respond_to do |format|
-      if @restaurant.update(restaurant_params)
-        format.html { redirect_to @restaurant, notice: "Restaurant was successfully updated." }
-        format.json { render :show, status: :ok, location: @restaurant }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
-      end
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant, notice: 'Restaurant was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /restaurants/1 or /restaurants/1.json
+  # DELETE /restaurants/1
   def destroy
     @restaurant.destroy
-    respond_to do |format|
-      format.html { redirect_to restaurants_url, notice: "Restaurant was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.'
   end
 
+
+
   private
+
+
     # Use callbacks to share common setup or constraints between actions.
     def set_restaurant
       @restaurant = Restaurant.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+    # Only allow a trusted parameter "white list" through.
     def restaurant_params
       params.require(:restaurant).permit(:name, :address, :phone_number, :category)
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Cleaning the DB"
+
+Restaurant.destroy_all
+
+puts "Seeding the DB"
+CATEGORY = %w((chinese italian japanese french belgian))
+5.times do
+  Restaurant.create(name: Faker::Mountain.name, address: Faker::Space.galaxy, category: CATEGORY.sample)
+  puts "created restaurants"
+end
+
+puts "done!"
